8c138998aabe4149082249a520f7e776db9bae6a
k0ffeee <cesarfernandocarlos@gmail.com>
2024-06-10 11:16:26 -0300

    order, array, parameters

---
+++ src/test/java/br/com/fernando/MethodOrderByOrderTest.java
@@ -0,0 +1,33 @@
+package br.com.fernando;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.Random.class)
+class MethodOrderRandonlyTest {
+
+    @Test
+    void testA(){
+        System.out.println("Running test A");
+    }
+
+    @Test
+    void testB(){
+        System.out.println("Running test B");
+    }
+
+    @Test
+    void testC(){
+        System.out.println("Running test C");
+    }
+
+    @Test
+    void testD(){
+        System.out.println("Running test D");
+    }
+
+
+}
